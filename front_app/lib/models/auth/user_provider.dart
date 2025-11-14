e5127dd3a281f34c555bddea8297eaf1b52a8d65
seoyeon <syn815640@gmail.com>
2025-11-15 00:21:09 +0900

    feat(auth): Refactor UserNotifier

--- front_app/lib/models/auth/user_provider.dart
+++ front_app/lib/models/auth/user_provider.dart
@@ -4,7 +4,6 @@ import 'package:deepflect_app/services/auth_service.dart';
 
 part 'user_provider.g.dart';
 
-// 사용자 정보만을 위한 별도 Provider
 @riverpod
 class UserNotifier extends _$UserNotifier {
   late final AuthService _authService;
@@ -12,10 +11,9 @@ class UserNotifier extends _$UserNotifier {
   @override
   UserInfo? build() {
     _authService = AuthService();
-    return null; // 초기값은 null
+    return null;
   }
 
-  // 사용자 정보 가져오기 (캐싱됨)
   Future<UserInfo?> fetchUserInfo() async {
     try {
       final userInfo = await _authService.getMe();
@@ -27,28 +25,23 @@ class UserNotifier extends _$UserNotifier {
     }
   }
 
-  // 사용자 정보 초기화
   void clearUserInfo() {
     state = null;
   }
 
-  // 사용자 정보 업데이트 (로그인 시)
   void updateUserInfo(UserInfo userInfo) {
     state = userInfo;
   }
 }
 
-// 사용자 정보 상태만 제공 (캐싱됨)
 @riverpod
 Future<UserInfo?> userInfo(UserInfoRef ref) async {
   final userNotifier = ref.watch(userNotifierProvider.notifier);
   final currentUser = ref.watch(userNotifierProvider);
   
-  // 이미 사용자 정보가 있으면 그대로 반환 (캐싱)
   if (currentUser != null) {
     return currentUser;
   }
   
-  // 없으면 새로 가져오기
   return await userNotifier.fetchUserInfo();
 } 
---
+++ front_app/lib/models/auth/user_provider.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userInfoHash() => r'921a4def89948f19dcd4626fa3d3411f388e504b';
+
+/// See also [userInfo].
+@ProviderFor(userInfo)
+final userInfoProvider = AutoDisposeFutureProvider<UserInfo?>.internal(
+  userInfo,
+  name: r'userInfoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userInfoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UserInfoRef = AutoDisposeFutureProviderRef<UserInfo?>;
+String _$userNotifierHash() => r'9b10c133ddb6a5aa570c057febf42ccd2338e326';
+
+/// See also [UserNotifier].
+@ProviderFor(UserNotifier)
+final userNotifierProvider =
+    AutoDisposeNotifierProvider<UserNotifier, UserInfo?>.internal(
+  UserNotifier.new,
+  name: r'userNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserNotifier = AutoDisposeNotifier<UserInfo?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
